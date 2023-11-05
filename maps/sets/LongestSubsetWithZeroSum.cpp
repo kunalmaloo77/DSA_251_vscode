c1c3a25fcb7998e5645308d9ea6e59502e8d9bfe
Kunal Maloo <kunnalmaloo@gmail.com>
2023-11-05 22:03:13 +0530

    fix in maps

--- maps/sets/LongestSubsetWithZeroSum.cpp
+++ maps/sets/LongestSubsetWithZeroSum.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 #include<unordered_map>
+#include <vector>
+
+using namespace std;
+
+int LongestSubsetWithZeroSum(vector <int> arr) {
+    int sum =0;
+    int max_count = 0;
+    unordered_map<int,int> map;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum+=arr[i];
+        if(sum == 0){
+            max_count = i+1;
+        }
+        if(map.find(sum)!=map.end()){
+            max_count = max(max_count,i-map[sum]);
+        }
+        else{
+            map[sum] = i;
+        }
+    }
+    return max_count;
+}
 using namespace std;
 int main(){
-    
+    vector<int> array = {22, -22, 35 ,76 ,87 ,-24 ,0 ,0, 24, 1, 0 ,0};
+    cout << LongestSubsetWithZeroSum(array);
     return 0;
 }
---
+++ mixed_questions/check_permutation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool areAnagram(string &str1, string &str2){
+    if (str1.length() != str2.length())
+    {
+        return false;
+    }
+    
+    for (int i = 0; i < str1.length(); i++)
+    {
+        
+    }
+    
+}
+int main(){
+    string s1 = "listen";
+    string s2 = "silent";
+    return 0;
+}
